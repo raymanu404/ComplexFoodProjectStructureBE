d3f74fc4cd1eee42dad7bc267a2d0790e47ce584
raymanu404 <manu.caprariu.brediceanu@gmail.com>
2024-05-07 18:30:15 +0300

    add default datetime on product

--- Domain/Models/Shopping/Product.cs
+++ Domain/Models/Shopping/Product.cs
@@ -13,8 +13,8 @@ namespace Domain.Models.Shopping
         public string Description { get; set; } = null!;
         public Price Price { get; set; }
         public string Image { get; set; } = null!;
-        public DateTime DateCreated { get; set; }
-        public DateTime DateUpdated { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateUpdated { get; set; } = DateTime.Now;
         public bool IsInStock { get; set; }
 
         //one to many
