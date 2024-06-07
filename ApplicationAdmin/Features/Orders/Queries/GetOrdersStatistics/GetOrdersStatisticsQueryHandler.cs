8168d79366262440720c09485d4f43add0d9feec
raymanu404 <manu.caprariu.brediceanu@gmail.com>
2024-06-07 15:45:28 +0300

    recalculate orders statistics

--- ApplicationAdmin/Features/Orders/Queries/GetOrdersStatistics/GetOrdersStatisticsQueryHandler.cs
+++ ApplicationAdmin/Features/Orders/Queries/GetOrdersStatistics/GetOrdersStatisticsQueryHandler.cs
@@ -3,18 +3,17 @@ using AutoMapper;
 using HelperLibrary.Constants;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using static ApplicationAdmin.Features.Orders.Queries.GetOrdersStatistics.Response;
 
 namespace ApplicationAdmin.Features.Orders.Queries.GetOrdersStatistics;
 public class GetOrdersStatisticsQueryHandler : IRequestHandler<GetOrdersStatisticsQuery, Response>
 {
     private readonly IUnitOfWorkAdmin _unitOfWork;
-    private readonly IMapper _mapper;
     const double withTva = 1 - Constants.TVA;
 
-    public GetOrdersStatisticsQueryHandler(IUnitOfWorkAdmin unitOfWork, IMapper mapper)
+    public GetOrdersStatisticsQueryHandler(IUnitOfWorkAdmin unitOfWork)
     {
         _unitOfWork = unitOfWork;
-        _mapper = mapper;
     }
     public async Task<Response> Handle(GetOrdersStatisticsQuery request, CancellationToken cancellationToken)
     {
@@ -28,16 +27,34 @@ public class GetOrdersStatisticsQueryHandler : IRequestHandler<GetOrdersStatisti
             .Where(o => o.DatePlaced >= startDate && o.DatePlaced <= endDate)
             .ToListAsync(cancellationToken);
 
+        var totalOrdersList = await _unitOfWork.Orders.GetQueryable()
+            .Include(o => o.OrderItems)
+            .ThenInclude(oi => oi.Product)
+            .ToListAsync(cancellationToken);
+
         var totalMerchantPrice = orders
             .SelectMany(o => o.OrderItems)
             .Sum(oi => oi.Cantity.Value * oi.Product.MerchantPrice.Value);
+
+
+        var totalDataMerchantPrice = totalOrdersList
+                    .SelectMany(o => o.OrderItems)
+                    .Sum(oi => oi.Cantity.Value * oi.Product.MerchantPrice.Value);
+
 
         var totalOrders = orders.Count;
         var totalPrice = orders.Sum(o => o.TotalPrice.Value);
         var totalProfitWithoutVTA = totalPrice - totalMerchantPrice;
         var totalProfitWithVTA = totalProfitWithoutVTA * withTva;
 
-        return new Response()
+
+        var totalDataOrders = totalOrdersList.Count;
+        var totalDataPrice = totalOrdersList.Sum(o => o.TotalPrice.Value);
+        var totalDataProfitWithoutVTA = totalDataPrice - totalDataMerchantPrice;
+        var totalDataProfitWithVTA = totalDataProfitWithoutVTA * withTva;
+
+
+        var dataInPeriodOfTime = new DataInPeriodOfTime()
         {
             TotalOrders = totalOrders,
             TotalPrice = totalPrice,
@@ -45,5 +62,44 @@ public class GetOrdersStatisticsQueryHandler : IRequestHandler<GetOrdersStatisti
             TotalProfitWithoutVTA = totalProfitWithoutVTA,
             TotalMerchantPrice = totalMerchantPrice
         };
+
+        var totalData = new TotalData()
+        {
+            TotalOrders = totalDataOrders,
+            TotalPrice = totalDataPrice,
+            TotalProfitWithVTA = totalDataProfitWithVTA,
+            TotalProfitWithoutVTA = totalDataProfitWithoutVTA,
+            TotalMerchantPrice = totalDataProfitWithVTA
+        };
+
+        var percentages = CalculatePercentages(dataInPeriodOfTime, totalData);
+
+        return new Response()
+        {
+            DataInPeriodOfTimeResponse = dataInPeriodOfTime,
+            TotalDataResponse = totalData,
+            DataInPercentsResponse = percentages
+        };
+    }
+
+
+    public static DataInPercents CalculatePercentages(DataInPeriodOfTime part, TotalData total)
+    {
+        var result = new DataInPercents
+        {
+            TotalOrders = CalculatePercentage(part.TotalOrders, total.TotalOrders),
+            TotalPrice = CalculatePercentage(part.TotalPrice, total.TotalPrice),
+            TotalMerchantPrice = CalculatePercentage(part.TotalMerchantPrice, total.TotalMerchantPrice),
+            TotalProfitWithoutVTA = CalculatePercentage(part.TotalProfitWithoutVTA, total.TotalProfitWithoutVTA),
+            TotalProfitWithVTA = CalculatePercentage(part.TotalProfitWithVTA, total.TotalProfitWithVTA)
+        };
+
+        return result;
+    }
+
+    private static double CalculatePercentage(double part, double total)
+    {
+        if (total == 0) return 0;
+        return Math.Round((part / total) * 100, 2);
     }
 }
--- ApplicationAdmin/Features/Orders/Queries/GetOrdersStatistics/Response.cs
+++ ApplicationAdmin/Features/Orders/Queries/GetOrdersStatistics/Response.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ApplicationAdmin.Features.Orders.Queries.GetOrdersStatistics;
 public class Response
 {
-    public int TotalOrders { get; set; }
-    public double TotalPrice { get; set; }
-    public double TotalMerchantPrice { get; set; }
-    public double TotalProfitWithoutVTA { get; set; }
-    public double TotalProfitWithVTA { get; set; }
+    public DataInPercents DataInPercentsResponse { get; set; } = null!;
+    public DataInPeriodOfTime DataInPeriodOfTimeResponse { get; set; } = null!;
+    public TotalData TotalDataResponse { get; set; } = null!;
 
+    public class DataInPercents
+    {
+        public double TotalOrders { get; set; }
+        public double TotalPrice { get; set; }
+        public double TotalMerchantPrice { get; set; }
+        public double TotalProfitWithoutVTA { get; set; }
+        public double TotalProfitWithVTA { get; set; }
+    }
+
+    public class DataInPeriodOfTime
+    {
+        public int TotalOrders { get; set; }
+        public double TotalPrice { get; set; }
+        public double TotalMerchantPrice { get; set; }
+        public double TotalProfitWithoutVTA { get; set; }
+        public double TotalProfitWithVTA { get; set; }
+    }
+    public class TotalData
+    {
+        public int TotalOrders { get; set; }
+        public double TotalPrice { get; set; }
+        public double TotalMerchantPrice { get; set; }
+        public double TotalProfitWithoutVTA { get; set; }
+        public double TotalProfitWithVTA { get; set; }
+    }
 }
